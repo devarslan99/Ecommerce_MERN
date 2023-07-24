26eb43523f7f060d18a574b2158ff8d81a63961e
devarslan99 <devarslan99@gmail.com>
2023-07-24 12:45:17 +0500

    Navigation menu and pages design

---
+++ client/src/components/Layouts/Layout.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+const Layout = ({children}) => {
+  return (
+  
+    <div>
+        <Header/>
+        <main style={{minHeight:"70vh"}}>
+
+        {children}
+
+        </main>
+
+    <Footer/>
+        
+        
+    </div>
+  )
+}
+
+export default Layout
